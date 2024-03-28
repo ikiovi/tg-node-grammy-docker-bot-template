4865ee01d8b1bc5ffbf46482e66dd8743d530c33
ikiovi <52862362+ikiovi@users.noreply.github.com>
2024-03-28 19:08:45 +0300

    Add `tracer` as logger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { Bot, session } from 'grammy';
 import { I18n } from '@grammyjs/i18n';
 import { MyContext } from './types/context';
+import { logger } from './utils/logger';
 
 const token = process.env.TOKEN;
 if (!token) throw new Error('TOKEN must be provided!');
@@ -21,12 +22,7 @@ bot.on('message').command('start', ctx => {
     ctx.reply(ctx.t('greeting', { userName: ctx.from.username ?? ctx.from.first_name }));
 });
 
-bot.catch(err => {
-    // Handle errors here
-    const { constructor: { name }, message } = <Error>err;
-    console.error(`[${new Date().toLocaleString('uk')}] \x1b[41m${name}\x1b[0m | ${message}`);
-});
-
+bot.catch(err => logger.error(err));
 bot.start({ drop_pending_updates: true });
 
 process.once('SIGINT', bot.stop);
